dff5f6be9315914d35d7a8e43c034a9771f0d624
idris <kulubiidris@gmail.com>
2025-01-13 00:54:50 +0300

    Add Drizzle ORM and PostgreSQL configuration; create initial database and layout components
    - Introduced `drizzle.config.ts` for Drizzle ORM configuration with PostgreSQL.
    - Added `drizzle.ts` to establish a database connection using Drizzle ORM.
    - Created initial database schema in `schema.ts`.
    - Updated `package.json` and `package-lock.json` to include `drizzle-orm`, `@vercel/postgres`, and related dependencies.
    - Implemented new layout components in `layout.tsx` for both authentication and root layouts.
    - Added loading and home page components to enhance the application structure.
    
    This commit sets up the database layer and initial layout structure for the application.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { ThemeProvider } from "@/components/themes/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
